b64c00a5342c205c19981f0309aea127b47dfbba
Eoan O Dea <odeaeoan@gmail.com>
2020-02-11 13:54:24 +0000

    Installed and implemented router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
@@ -7,5 +8,6 @@ import 'vue-material/dist/theme/default.css'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
